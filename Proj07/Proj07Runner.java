f649ad8e9fe9f2703851dcb6788f4ba2189489b2
Austin Riggs <ariggs7@gmail.com>
2023-12-12 11:59:49 -0600

    Add files via upload

---
+++ Proj07/Proj07Runner.java
@@ -0,0 +1,24 @@
+// Asg07
+// Austin Riggs
+// 03/29/23
+// ITSE-2321-001
+
+// Creation of the Proj07Runner class that extends the Proj07 class 
+class Proj07Runner extends Proj07  
+{
+	// Proj07 constructor
+	public Proj07Runner()
+	{
+		System.out.println(
+		  "\nI certify that this program is my own work\n" +
+          "and is not the work of others. I agree not\n" +
+          "to share my solution with others.\n");
+	}
+	
+    // Overrides Proj07 run method	
+	void run()
+	{
+		System.out.println("Austin Riggs \n");
+		
+	} // end run 
+} // end class Proj07Runner
