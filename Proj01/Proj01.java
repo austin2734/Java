fd7da5eeb4f4796aac0561278bb8dcd33c7ab3ed
Austin Riggs <ariggs7@gmail.com>
2023-12-12 11:58:54 -0600

    Add files via upload

---
+++ Proj01/Proj01.java
@@ -0,0 +1,19 @@
+// Asg01
+// Austin Riggs
+// 01/10/23
+// ITSE-2321-001
+
+class Proj01
+{
+  public static void main(String[] args)
+  {
+    System.out.println("I certify that this program is my own work");
+	System.out.println("and is not the work of others. I agree not");
+	System.out.println("to share my solution with others.");
+	System.out.println("Austin Riggs");
+	System.out.println("Hello World");
+	
+  } // end main method
+  
+}// end class
+
